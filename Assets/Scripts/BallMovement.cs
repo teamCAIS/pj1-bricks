8138640450303c1677b898adcaec1ee0aa1ceb8d
Ulisses Lopes <ulissesdvl@yahoo.com.br>
2019-10-29 02:10:45 -0300

    testando nova fisica, adicionando som e indo para game over se morrer 3 vezes

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,24 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallMovement : MonoBehaviour {
 
 	protected Rigidbody2D rb2d;
-	public float speedX = -5.0f;
-	public float speedY = 3.0f;
+	public float speed = 100.0f;
+	private bool onPlay = false;
+	private Vector2 initialPosition = new Vector2(0.0f,-4.3f);
+	public AudioClip clip;
+	public AudioSource source;
+
+	private int lifes = 3;
 
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
-	}
-	
-	void FixedUpdate () {
-		rb2d.velocity = new Vector2(speedX * Time.deltaTime * 10, speedY * Time.deltaTime * 10);
+		this.source.clip = this.clip;
 	}
 
+	void Update () {
+		if(Input.GetButtonUp("Jump") && !this.onPlay)
+		{
+			this.rb2d.AddForce(new Vector2(1.0f*speed,1.0f*speed));
+			this.onPlay = true;
+		}
+		if(this.lifes <= 0) {
+			SceneManager.LoadScene("GameOverScene");
+		}
+	}
+
+	void OnTriggerExit2D()
+    {
+        this.onPlay = false;
+        transform.position = this.initialPosition;
+        this.rb2d.velocity = Vector2.zero;
+				this.lifes--;
+				Debug.Log(this.lifes);
+    }
+	
+	void FixedUpdate () {
+		//rb2d.velocity = new Vector2(speedX * Time.deltaTime * 10, speedY * Time.deltaTime * 10);
+	}
+	
 	void OnCollisionEnter2D(Collision2D col){
+		source.Play();
+		if(col.gameObject.CompareTag("Brick")){
+			col.gameObject.SetActive(false);
+		}
+	}
+
+	/* void OnCollisionEnter2D(Collision2D col){
+		//normalmente se pega a tag e não o name, 
+		//pois o name é mais volúvel a mudanças, inclusive por designers
 		switch(col.gameObject.name){
 			case "Brick":
+				//creio ser prefirivel inativar ao invés de destruir, 
+				//para poder só ativar novamente se necessitar, melhorando a performance
 				Destroy(col.gameObject);
 				speedY *= -1;
 				break;
@@ -45,5 +83,5 @@ public class BallMovement : MonoBehaviour {
 				speedY *= -1;
 				break;
 		}
-	}
+	} */
 }
--- Assets/Scripts/BarMovement.cs
+++ Assets/Scripts/BarMovement.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class BarMovement : MonoBehaviour {
 
 	public float speed = 20.0f;
+	public float edge = 6.9f;
 	protected float move;
-	private Rigidbody2D rb2d; 
+	//private Rigidbody2D rb2d; 
 
 	void Start(){
-		rb2d = GetComponent<Rigidbody2D> ();
+		//rb2d = GetComponent<Rigidbody2D> ();
 	}
 
 	void FixedUpdate(){
-		move = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-
-		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)){
-			rb2d.position = rb2d.position + new Vector2(move, 0);
+		move = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+		transform.Translate(new Vector2(move,0));
+		if(transform.position.x < -this.edge){
+			transform.position = new Vector2(-this.edge,transform.position.y);
+		}
+		if(transform.position.x > this.edge){
+			transform.position = new Vector2(this.edge,transform.position.y);
 		}
 	}
 }
